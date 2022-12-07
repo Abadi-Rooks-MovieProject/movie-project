cf05b623762aede7df45d6e3e48b4d0a65258524
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-12-07 09:08:28 -0600

    cleaned up code

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
 
             // save the array back to the database using $.ajax()
             $.ajax({
-                url: "/saveArray",
+                url: "https://flannel-brick-list.glitch.me/movies",
                 type: "POST",
                 data: JSON.stringify(array),
                 contentType: "application/json; charset=utf-8",
@@ -79,22 +79,7 @@ $(document).ready(function () {
             });
         }
     });
-    const exampleModal = document.getElementById('exampleModal')
-    exampleModal.addEventListener('show.bs.modal', event => {
-        // Button that triggered the modal
-        const button = event.relatedTarget
-        // Extract info from data-bs-* attributes
-        const recipient = button.getAttribute('data-bs-whatever')
-        // If necessary, you could initiate an AJAX request here
-        // and then do the updating in a callback.
-        //
-        // Update the modal's content.
-        const modalTitle = exampleModal.querySelector('.modal-title')
-        const modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-        modalTitle.textContent = `New message to ${recipient}`
-        modalBodyInput.value = recipient
-    })
 });
 
 
